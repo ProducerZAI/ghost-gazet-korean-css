24bb2039a33638e3e3c9d0a63a22325c3ff6228a
ProducerZAI <zai.rev@gmail.com>
2026-07-12 10:23:31 +0900

    Update lang-header.js

--- lang-header.js
+++ lang-header.js
@@ -1,16 +1,3 @@
-<script>
-window.gtranslateSettings = {
-  "default_language": "ko",
-  "native_language_names": true,
-  "detect_browser_language": true,
-  "languages": ["ko", "en", "th", "tl", "km", "id", "ms", "ja", "zh-CN", "vi", "ar", "es", "pt", "ru"],
-  "wrapper_selector": ".gtranslate_wrapper",
-  "float_switcher_open_direction": "bottom",
-  "flag_size": 24
-}
-</script>
-<script src="https://cdn.gtranslate.net/widgets/latest/float.js" defer></script>
-<style>
 .gtranslate_wrapper .gt-current-lang span:not(.gt-flag),
 .gtranslate_wrapper [class*="gt"] span:not([class*="flag"]):not([class*="img"]) {
   display: none !important;
@@ -19,4 +6,3 @@ window.gtranslateSettings = {
   display: inline-flex !important;
   align-items: center;
 }
-</style>
